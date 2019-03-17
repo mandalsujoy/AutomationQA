33b5189adebbe1ac4333152f4152043a7eb2716d
SujoyM <SujoyM@RSLPTP862.rssoftware.com>
2019-03-17 21:30:28 +0530

    committing from office pc

--- AutomatedQA/src/main/java/JavaSample/Demo.java
+++ AutomatedQA/src/main/java/JavaSample/Demo.java
@@ -5,8 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 
 		System.out.println("demo");
-		System.out.println("updated from office pc");
 		System.out.println("updation1 from homepc");
+		System.out.println("updation from office pc");
 	}
 
 }

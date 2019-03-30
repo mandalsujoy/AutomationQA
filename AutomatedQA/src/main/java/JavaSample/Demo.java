706e0efd6639dabab2ca2b73511ceaff5c3f125f
SujoyM <SujoyM@RSLPTP862.rssoftware.com>
2019-03-30 08:24:50 +0530

    commit_03_30_19

--- AutomatedQA/src/main/java/JavaSample/Demo.java
+++ AutomatedQA/src/main/java/JavaSample/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 
-		System.out.println("demo");
+		
 		System.out.println("updation1 from homepc");
 		System.out.println("updation from office pc");
 	}

239ce9e35d76e110536e653da7145b01065e29af
sujoy <sujoymandal33@gmail.com>
2019-03-31 18:26:12 +0530

    commit 03_31_19 from home pc

--- AutomatedQA/src/main/java/JavaSample/Demo2.java
+++ AutomatedQA/src/main/java/JavaSample/Demo2.java
@@ -5,6 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 
 		System.out.println("this class is created from office pc!!");
+		System.out.println("this line is added from home pc!!");
 	}
 
 }

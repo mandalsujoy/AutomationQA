97610f0525685196af6dbb6b49b5d15ce32f382a
sujoy <sujoymandal33@gmail.com>
2019-03-31 18:30:25 +0530

    committing again from home pc

--- AutomatedQA/src/main/java/JavaSample/Demo3.java
+++ AutomatedQA/src/main/java/JavaSample/Demo3.java
@@ -5,6 +5,7 @@ public class Demo3 {
 	public static void main(String[] args) {
 		
 		System.out.println("this class is added from home pc!!");
+		System.out.println("this is added from home pc!!");
 
 	}
 
